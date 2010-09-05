fedaecfa1cb23dca2e7989cbb2b808ec666b95c5
evilrix <evilrix@9e80ea24-377e-4813-bbc1-9e97c512fa1a>
2010-09-05 10:19:21 +0000

    Refined property class
    git-svn-id: http://evilrix.unfuddle.com/svn/evilrix_evilrix@23 9e80ea24-377e-4813-bbc1-9e97c512fa1a

--- devtools/trunk/include/devtools/property.hpp
+++ devtools/trunk/include/devtools/property.hpp
@@ -4,13 +4,11 @@ class Property
 public:
 	typedef T value_type;
 	
-	Property(value_type vt = value_type()) : vt_(vt) {}
+	Property(value_type value = value_type()) : value_(value) {}
 	
-	// Unfortunately, due to the rules of C++ this must be overridden
-	// in the specialised class and then called explicitly.
-	Property<int> & operator = (value_type const & vt)
+	Property<int> & operator = (value_type const & value)
 	{
-		put(vt);
+		set(value);
 		return *this;
 	}
 	
@@ -20,60 +18,25 @@ public:
 	}
 	
 protected:
-	// Override these to add specialised validation
-	virtual value_type const & get() const { return vt_; };
-	virtual void put(value_type const & vt){ vt_ = vt; };
+	value_type const & get() const { return value_; };
+	void set(value_type const & value){ value_ = value; };
 
-	value_type vt_;
+	value_type value_;
 };
 
-#include <stdexcept>
+#define property_(TYPE, NAME, IMPL) \
+class NAME ## _ : public Property<TYPE> \
+	{ \
+	public: \
+		Property<int> & operator = (value_type const & value)	{ \
+			return Property<int>::operator=(value); \
+		} \
+	private: \
+		IMPL \
+	} NAME
 
-class MyClass
-{
-public:
+#define get_ \
+	value_type const & get() const
 
-	// A property definition
-	class Value_ : public Property<int>
-	{
-	public:
-		// Pass assignment on to the base class to handle
-		Property<int> & operator = (value_type const & vt)	{
-			return Property<int>::operator=(vt);
-		}
-		
-	private:
-		// Implement get() if you want to specialise the behavior
-		
-		void put(value_type const & vt) // Only allow 0 to 99
-		{
-			if(vt < 0 || vt > 99) {
-				throw std::runtime_error("validation error");
-			}
-			
-			Property<int>::put(vt);
-		}
-		
-	} Value; 
-};	
-
-#include <iostream>
-
-int main()
-{
-	MyClass myClass;
-	myClass.Value = 99;
-	
-	int foo = myClass.Value;
-	
-	std::cout << foo << std::endl;
-
-	try
-	{
-		myClass.Value = 100; // Fails validation
-	}
-	catch(std::exception const & e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-}
+#define set_ \
+	void set(value_type const & value)
