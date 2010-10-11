5b242f8e2dbf336fd78d731202e733e850515811
evilrix <evilrix@9e80ea24-377e-4813-bbc1-9e97c512fa1a>
2010-10-11 07:00:13 +0000

    Rename valuetype to value_type to be consistant with STL
    git-svn-id: http://evilrix.unfuddle.com/svn/evilrix_evilrix@49 9e80ea24-377e-4813-bbc1-9e97c512fa1a

--- devtools/trunk/include/devtools/property.hpp
+++ devtools/trunk/include/devtools/property.hpp
@@ -1,35 +1,35 @@
 #define property_(TYPE, OWNR, NAME, IMPL) \
-	private: \
-		class NAME ## __ \
-		{ \
-		friend class OWNR; \
-		public: \
-		  typedef NAME ## __ this_type; \
-   		typedef TYPE valuetype; \
-			NAME ## __ () {} \
-			explicit NAME ## __ (valuetype const & value) \
-				: NAME ## (value) {} \
-			IMPL \
-	   private: \
-		   valuetype NAME ##; \
-		}; \
-	public: \
-		NAME ## __ NAME;
+   private: \
+      class NAME ## __ \
+      { \
+      friend class OWNR; \
+      public: \
+         typedef NAME ## __ this_type; \
+         typedef TYPE value_type; \
+         NAME ## __ () {} \
+         explicit NAME ## __ (value_type const & value) \
+            : NAME ## (value) {} \
+         IMPL \
+      private: \
+         value_type NAME ##; \
+      }; \
+   public: \
+      NAME ## __ NAME;
 
 #define get_ \
-	operator valuetype const & () const \
-	{ \
-		return get(); \
-	} \
-	valuetype const & get() const
+   operator value_type const & () const \
+   { \
+      return get(); \
+   } \
+   value_type const & get() const
 
 #define set_ \
-	this_type & operator = (valuetype const & value) \
-	{ \
-		set(value); \
-		return *this; \
-	} \
-	void set(valuetype const & value)
+   this_type & operator = (value_type const & value) \
+   { \
+      set(value); \
+      return *this; \
+   } \
+   void set(value_type const & value)
 
 #define xprop_(NAME) \
-	NAME ## . ## NAME
+   NAME ## . ## NAME
